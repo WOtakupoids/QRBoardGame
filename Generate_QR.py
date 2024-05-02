106c98a0b47cea0ad24ff602b34793c71930300c
bacon <bacon0817.ix@gmail.com>
2024-05-02 15:46:51 +0900

    文字数識別追加

--- Generate_QR.py
+++ Generate_QR.py
@@ -1,18 +1,46 @@
 from pprint import pprint
 import re
 import numpy as np
-from QR_parameter import Generate_parameter
+from QR_parameter import Generate_parameter,QR_parameter
 import pandas as pd
 import os
 class QR():
-    def __init__(self,QR_parm:Generate_parameter) -> None:
+    def __init__(self,QR_parm:Generate_parameter):
         self.matrix=QR_parm.matrix
         self.QR_list=np.zeros((self.matrix,self.matrix))
         self.pram=QR_parm
+        self.QR_pram=QR_parameter()
     def generate_QR(self):
+        #モード設定
+        self.__set_mode()
+        #入力文字数がオーバーしてないかチェック
+        self.validate_inp()
+        self.__set_char_counter()
         return
-    def validate_inp(self,inp):
-        am = re.compile('[a-zA-Z]+')
+    def __check_capacity(self):
+        mode=self.QR_pram.mode
+        v_table=pd.read_csv('version_table.csv')
+        v1=v_table[v_table['version']==self.pram.versinon]
+        v1=v1[v1["ECL"]==self.pram.error_level]
+        if mode==0b0001:
+            v1=v1['NM']
+        elif mode==0b0010:
+            v1=v1['AL']
+        else:
+            v1=v1['8B']
+        return v1.values
+    def validate_inp(self):
+        self.__set_mode()
+        capa=self.__check_capacity()[0]
+        if len(self.pram.input)>capa:
+            print("Capacity over!!!")
+            return
+        else:
+            print("mode:"+str(self.QR_pram.mode))
+            print(str(len(self.pram.input))+'/'+str(capa))
+    def __set_mode(self):
+        inp=self.pram.input
+        am = re.compile('[a-zA-Z0-9]+')
         nm = re.compile('[0-9]+')
         mode:int=0b0001
         if nm.fullmatch(inp):
@@ -21,8 +49,18 @@ class QR():
             mode=0b0010
         else:
             mode=0b0100
-        v_table=pd.read_csv('version_table.csv')
-        v_table[]
+        self.QR_pram.mode=mode
+    def __set_char_counter(self):
+        mode=self.QR_pram.mode
+        counter=len(self.pram.input)
+        if mode==0b0001:
+            counter=format(counter,'010b')
+
+        elif mode==0b0010:
+            counter=format(counter,'09b')
+        else:
+            counter=format(counter,'08b')
+        print(counter)
     def add_symbol(self):
         symbol=np.ones((7,7))
         symbol[1:6,1:6]=0
@@ -30,7 +68,6 @@ class QR():
         self.QR_list[0:7,0:7]=symbol
         self.QR_list[0:7,-7:]=symbol
         self.QR_list[-7:,0:7]=symbol
-        
     def get_tui_QR(self):
         l1=""
         for i in self.QR_list:
@@ -42,9 +79,13 @@ class QR():
             l1+="\n"
         return l1
 if __name__=="__main__":
-    Qp=Generate_parameter("a")
+
+    print()
+    Qp=Generate_parameter("ああああ")
     Qp.versinon=1
     qr=QR(Qp)
-    qr.add_symbol()
+    qr.generate_QR()
+    #qr.validate_inp()
+    #qr.add_symbol()
     #print(qr.get_tui_QR())
     #print(qr.QR_list)
--- QR_parameter.py
+++ QR_parameter.py
@@ -1,9 +1,10 @@
 class Generate_parameter():
-    def __init__(self,inp,version:int=1,error_level:int=1) -> None:
+    def __init__(self,inp,version:int=1,error_level:str="H") -> None:
         self.versinon:int=version
         self.matrix:int=(self.versinon-1)*4+21
-        self.error_level:int=error_level
+        self.error_level:str=error_level
         self.input=inp
 class QR_parameter():
     def __init__(self) -> None:
         self.mode:int=0b0001
+        self.char_counter:int=0b0
